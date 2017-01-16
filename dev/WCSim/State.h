40b07c3264c816c203433f2c3a171977f953076b
ptibbets <misio4x4@gmail.com>
2017-01-16 22:10:58 +0100

    Added needed operators for State

--- dev/WCSim/State.h
+++ dev/WCSim/State.h
@@ -11,6 +11,78 @@ namespace wcsim
     public:
         State() = default;
 
+        State & operator+(float vCoef)
+        {
+            for(std::size_t aIndex = 0; aIndex < mState.size(); aIndex++)
+            {
+                mState[aIndex] += vCoef;
+            }
+            return *this;
+        }
+
+        State & operator+(State & vOther)
+        {
+            for(std::size_t aIndex = 0; aIndex < mState.size(); aIndex++)
+            {
+                mState[aIndex] += vOther[aIndex];
+            }
+            return *this;
+        }
+
+        State & operator-(float vCoef)
+        {
+            for(std::size_t aIndex = 0; aIndex < mState.size(); aIndex++)
+            {
+                mState[aIndex] -= vCoef;
+            }
+            return *this;
+        }
+
+        State & operator-(State & vOther)
+        {
+            for(std::size_t aIndex = 0; aIndex < mState.size(); aIndex++)
+            {
+                mState[aIndex] -= vOther[aIndex];
+            }
+            return *this;
+        }
+
+        State & operator*(float vCoef)
+        {
+            for(std::size_t aIndex = 0; aIndex < mState.size(); aIndex++)
+            {
+                mState[aIndex] *= vCoef;
+            }
+            return *this;
+        }
+
+        State & operator*(State & vOther)
+        {
+            for(std::size_t aIndex = 0; aIndex < mState.size(); aIndex++)
+            {
+                mState[aIndex] *= vOther[aIndex];
+            }
+            return *this;
+        }
+
+        State & operator/(float vCoef)
+        {
+            for(std::size_t aIndex = 0; aIndex < mState.size(); aIndex++)
+            {
+                mState[aIndex] /= vCoef;
+            }
+            return *this;
+        }
+
+        State & operator/(State & vOther)
+        {
+            for(std::size_t aIndex = 0; aIndex < mState.size(); aIndex++)
+            {
+                mState[aIndex] /= vOther[aIndex];
+            }
+            return *this;
+        }
+
         float & operator[](std::size_t vIndex)
         {
             return mState[vIndex];
